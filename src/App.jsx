104fcaee2c299082495cbfefb01257c453cb1b75
Suraj <kumarsuraj54057@gmail.com>
2025-05-22 14:27:36 +0530

    refactor: simplify App component by using AppRoutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,5 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Profile from './pages/Profile';
+import AppRoutes from "./routes/AppRoutes";
 
-const ProtectedRoute = ({ children }) => {
-    const user = JSON.parse(localStorage.getItem('user'));
-    if (!user) {
-        return <Navigate to="/login" replace />;
-    }
-    return children;
-};
-
-function App() {
-    return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route
-                path="/profile"
-                element={
-                    <ProtectedRoute>
-                        <Profile />
-                    </ProtectedRoute>
-                }
-            />
-        </Routes>
-    );
-}
+const App = () => <AppRoutes />;
 
 export default App;
